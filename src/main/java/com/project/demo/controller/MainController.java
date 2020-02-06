9ba8ba31a434d58e3095237ee125621a6356114e
Nurbek70 <58589195+Nurbek70@users.noreply.github.com>
2020-02-06 10:28:54 +0600

    Закончил

--- src/main/java/com/project/demo/controller/MainController.java
+++ src/main/java/com/project/demo/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.project.demo.controller;
 
 import com.project.demo.entities.Categories;
+import com.project.demo.entities.Toys;
 import com.project.demo.repositories.CategoriesRepository;
+import com.project.demo.repositories.ToysRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -19,6 +21,9 @@ public class MainController {
     @Autowired
     private CategoriesRepository categoriesRepository;
 
+    @Autowired
+    private ToysRepository toysRepository;
+
     @GetMapping(path="/")
     public String index(ModelMap model){
 //        categoriesRepository.save(new Categories(null, "Copter"));//Checking
@@ -26,6 +31,9 @@ public class MainController {
 //        for(Categories c : categories)
 //            System.out.println(c.getName());
         model.addAttribute("categories", categories);
+        List<Toys> toys = toysRepository.findAll();
+        model.addAttribute("toys", toys);
+
         return "index";
     }
 
@@ -59,6 +67,58 @@ public class MainController {
             categoryOzgert.setName(category);
             categoriesRepository.save(categoryOzgert);
         }
+        return "redirect:/";
+    }
+
+    @PostMapping(path = "/addToy")
+    public String addToy(@RequestParam(name = "toy") String toy,
+//                         @RequestParam(name = "toyCategory") String toyCategory,
+                         @RequestParam(name = "price") int price,
+                         @RequestParam(name = "toyCategoryId") Long toyCategoryId){
+
+        Optional<Categories> neededCategory = categoriesRepository.findById(toyCategoryId);
+        if(neededCategory.isPresent()){
+            Categories category = neededCategory.get();
+            Toys newToy = new Toys(null, toy, price, category);
+            toysRepository.save(newToy);
+        }
+
+//        Toys newToy = new Toys(null, toy, price, categoriesRepository.findById(toyCategoryId).get());
+//        toysRepository.save(newToy);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(path = "/editToyPage/{id}")
+    public String editToyPage(ModelMap model, @PathVariable(name = "id") Long id){
+        Optional<Toys> toy = toysRepository.findById(id);
+        model.addAttribute("toy", toy.orElse(new Toys(null,"No name",0,null)));
+
+        List<Categories> categories = categoriesRepository.findAll();
+        //model.addAttribute("category", category.orElse(new Categories(null,"No name")));
+        model.addAttribute("categories", categories);
+
+        return "editToyPage";
+    }
+
+    @PostMapping(path = "/editToy")
+    public String editToy(@RequestParam(name = "id") Long id,
+                          @RequestParam(name = "toy") String toy,
+                          @RequestParam(name = "toyCategoryId") Long toyCategoryId,
+                          @RequestParam(name = "price") double price){
+
+        Optional<Categories> category = categoriesRepository.findById(toyCategoryId);
+
+        Optional<Toys> neededToy = toysRepository.findById(id);
+        if(neededToy.isPresent()){
+            neededToy.get().setName(toy);
+            neededToy.get().setCategories(category.get());
+            neededToy.get().setPrice(price);
+
+            toysRepository.save(neededToy.get());
+        }
+
+
         return "redirect:/";
     }
 }
--- src/main/java/com/project/demo/repositories/ToysRepository.java
+++ src/main/java/com/project/demo/repositories/ToysRepository.java
@@ -1,4 +1,13 @@
 package com.project.demo.repositories;
 
-public interface ToysRepository {
+import com.project.demo.entities.Categories;
+import com.project.demo.entities.Toys;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ToysRepository extends JpaRepository<Toys, Long> {
+    List<Toys> findAll();
+    Optional<Toys> findById(Long id);
 }
